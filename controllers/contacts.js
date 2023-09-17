31c8fd2651b1b3ea6e83ae006b1f12d434309d49
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2023-09-17 17:09:22 +0300

    ctrlWrapper

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,5 +1,6 @@
 const contacts = require("../models/contacts");
 const HttpError = require("../helpers/HttpError");
+const ctrlWrapper = require("../helpers/ctrlWrapper");
 const Joi = require("joi");
 
 const addSchema = Joi.object({
@@ -8,85 +9,65 @@ const addSchema = Joi.object({
   phone: Joi.string().required(),
 });
 
-const listContacts = async (req, res, next) => {
-  try {
-    const allContacts = await contacts.listContacts();
-    res.json(allContacts);
-  } catch (error) {
-    next(error);
-  }
+const listContacts = async (req, res) => {
+  const allContacts = await contacts.listContacts();
+  res.json(allContacts);
 };
 
-const getContactById = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.getContactById(contactId);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
   }
+  res.json(result);
 };
 
-const addContact = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      const message = `missing required ${error.details[0].path[0]} field`;
-      throw HttpError(400, message);
-    }
-    const result = await contacts.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
+const addContact = async (req, res) => {
+  const { error } = addSchema.validate(req.body);
+  if (error) {
+    const message = `missing required ${error.details[0].path[0]} field`;
+    throw HttpError(400, message);
   }
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
 };
 
-const removeContact = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.removeContact(contactId);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-
-    res.json({
-      message: "contact deleted",
-    });
-  } catch (error) {
-    next(error);
+const removeContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
   }
+
+  res.json({
+    message: "contact deleted",
+  });
 };
 
-const updateContact = async (req, res, next) => {
-  try {
-    const emptyBody = Object.keys(req.body).length === 0;
-    if (emptyBody) {
-      throw HttpError(400, "missing fields");
-    }
-
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      const message = `missing required ${error.details[0].path[0]} field`;
-      throw HttpError(400, message);
-    }
-    const { contactId } = req.params;
-    const result = await contacts.updateContact(contactId, req.body);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const updateContact = async (req, res) => {
+  const emptyBody = Object.keys(req.body).length === 0;
+  if (emptyBody) {
+    throw HttpError(400, "missing fields");
   }
+
+  const { error } = addSchema.validate(req.body);
+  if (error) {
+    const message = `missing required ${error.details[0].path[0]} field`;
+    throw HttpError(400, message);
+  }
+  const { contactId } = req.params;
+  const result = await contacts.updateContact(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
 };
 
 module.exports = {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
+  listContacts: ctrlWrapper(listContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  removeContact: ctrlWrapper(removeContact),
+  updateContact: ctrlWrapper(updateContact),
 };
