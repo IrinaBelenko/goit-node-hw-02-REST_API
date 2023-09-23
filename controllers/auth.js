bf1fd519cabfd7321bd84719545efc54b47cd670
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2023-09-23 20:36:55 +0300

    unique email

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,12 +3,12 @@ const ctrlWrapper = require("../helpers/ctrlWrapper");
 const { User } = require("../models/user");
 
 const register = async (req, res) => {
-  //   const { email, password } = req.body;
-  //   const user = await User.findOne({ email });
-  //   if (user) {
-  //     throw HttpError(409, "Email already in use");
-  //   }
-  //   const hashPassword = await bcrypt.hash(password, 10);
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
+  const hashPassword = await bcrypt.hash(password, 10);
   //  const newUser = await User.create({ ...req.body, password: hashPassword });
   const newUser = await User.create(req.body);
   res.status(201).json({
